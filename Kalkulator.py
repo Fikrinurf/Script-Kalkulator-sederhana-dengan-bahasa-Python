c69c065011dbba3b1f50a256da6ba6b4f3751fb9
Fikri Nur Firdaus <104166568+Fikrinurf@users.noreply.github.com>
2022-05-27 17:31:57 +0700

    script
    g

---
+++ Kalkulator.py
@@ -0,0 +1,30 @@
+## membuat kalkulator sederhana dengan bahasa pemograman python
+## dengan pengkondisian else if
+
+
+print("===========================================")
+print("Program Kalkulator Sederhana dengan Else If")
+print("===========================================")
+
+print("\n")
+
+
+angkaPertama = float(input("Masukan angka Pertama :"))
+operator = input("Masukan operator (+,-,x,/) :")
+angkaKedua = float(input("Masukan angka Kedua :"))
+
+
+if operator == '+': 
+    hasil = angkaPertama + angkaKedua
+    print(f"hasil dari {angkaPertama} + {angkaKedua} adalah :{hasil}")
+elif operator == '-':
+    hasil = angkaPertama - angkaKedua
+    print(f"hasil dari {angkaPertama} - {angkaKedua} adalah :{hasil}")
+elif operator == 'x':
+    hasil = angkaPertama * angkaKedua
+    print(f"hasil dari {angkaPertama} x {angkaKedua} adalah :{hasil}")
+elif operator == '/':
+    hasil = angkaPertama / angkaKedua
+    print(f"hasil dari {angkaPertama} / {angkaKedua} adalah :{hasil}")
+else:
+    print("Anda salah memasukan operatorrrrr Kisamaaaaaaaaaaaaaaaaaaaaaaaa!!!!!")
